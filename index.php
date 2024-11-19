0df0d838d2bbb597611a1c88c7082fee3d77ad88
lauracasttro <laura.casttro8@gmail.com>
2024-11-19 11:41:04 -0300

    Commit da Laura

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
     <head>
         <meta utf="utf8_decode">
         <title> Calculadora Simples </title>
+        <link rel="stylesheet" href="style.css">
     </head>
-    <body>
+    <body class="p1">
         <h1> Calculadora Simples </h1>
-        <form method="GET" action="/calc/">
+        <form method="GET" action="/aula1911/calc/" class="p2">
             <label for="num1">Num1:</label><br>
             <input type="text" name="n1"><br>
             <label for="num2">Num2:</label><br>
@@ -14,7 +15,11 @@
             <fieldset style="margin-right: 1000;">                
                 <legend>Operações</legend>
                 <input type="radio" name="op" value="soma" checked>Soma</input><br>
-                <input type="radio" name="op" value="subtração">Subtração</input>
+                <input type="radio" name="op" value="subtração" checked>Subtração</input><br>
+                <input type="radio" name="op" value="multiplicaçao" checked>multiplicaçao</input><br>
+                <input type="radio" name="op" value="divisão" checked>divisão</input><br>
+
+        
             </fieldset>
         </form>
         <?php
@@ -23,7 +28,7 @@
                 if(isset($_GET['n1']) and isset($_GET['n2'])){
                     $n1 = $_GET['n1'];
                     $n2 = $_GET['n2'];
-        
+                }
             
                     function soma(){
                         return $GLOBALS['n1'] + $GLOBALS['n2'];
@@ -32,14 +37,24 @@
                     function subtracao(){
                         return $GLOBALS['n1'] - $GLOBALS['n2'];
                     }
+
+                    function multiplicaçao(){
+                        return $GLOBALS['n1'] * $GLOBALS['n2'];
+                    }
+
+                    function divisão(){
+                        return $GLOBALS['n1'] % $GLOBALS['n2'];
+                    }
        
                     if($_GET['op'] == 'soma'){
                         echo "<h1> $n1  + $n2 = " .soma(). "</h1>";
-                    } else {
+                    } else if($_GET['op'] == 'subtraçao'){
                         echo "<h1> $n1 - $n2 = " .subtracao(). "</h1>";
+                    } else if($_GET['op'] == 'multiplicaçao'){
+                        echo "<h1> $n1 * $n2 = " .multiplicaçao(). "</h1>";
+                    } else if($_GET['op'] == 'divisão'){
+                        echo "<h1> $n1 % $n2 = " .divisão(). "</h1>";
                     }
-
-                }
             }
 
         ?>
